415559a29a095b5507ce45486103f830edd92192
yago <yagocam5@gmail.com>
2024-04-21 13:42:09 -0300

    melhorias

--- app/Http/Requests/CreatePostRequest.php
+++ app/Http/Requests/CreatePostRequest.php
@@ -9,10 +9,7 @@ class CreatePostRequest extends FormRequest
     /**
      * Determine if the user is authorized to make this request.
      */
-    public function authorize(): bool
-    {
-        return auth()->check();
-    }
+  
 
     /**
      * Get the validation rules that apply to the request.
