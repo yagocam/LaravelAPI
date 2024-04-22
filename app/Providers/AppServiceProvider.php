0c3c6bc150e96d292bd638e5ee22fbb68682f80a
yago <yagocam5@gmail.com>
2024-04-22 15:34:16 -0300

    hotfix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,8 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if(env('APP_ENV') !== 'local') {
-            URL::forceScheme('https');
-         }
+       //
     }
 }
