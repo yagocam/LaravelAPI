0d68005b4150e573264f5762a4f7cdc998ad0a3e
yago <yagocam5@gmail.com>
2024-04-22 15:07:22 -0300

    url

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use App\Repositories\PostRepository;
 use App\Repositories\Contracts\PostRepositoryInterface;
-
+use Illuminate\Support\Facades\URL;
 class AppServiceProvider extends ServiceProvider
 {
     /**
