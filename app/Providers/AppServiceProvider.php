d1fa7212160a4ad3ae9939312a8b7803da5d95d1
yago <yagocam5@gmail.com>
2024-04-22 14:43:37 -0300

    contracts and dependency injection

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use App\Repositories\PostRepository;
-use App\Repositories\PostRepositoryInterface;
+use App\Repositories\Contracts\PostRepositoryInterface;
 
 class AppServiceProvider extends ServiceProvider
 {
